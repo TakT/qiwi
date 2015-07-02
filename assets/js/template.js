c33f54a691e871b89724a82fed9361448bd7aaca
Кирилл <kirill.bereza@yandex.ru>
2015-07-03 01:47:13 +0600

    Fix country select

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function() {
-
-    jQuery('select').selectric().on('change', function(e) {
-        console.log(e);
-        console.log($(this).val());
-
-        var countryChangerHtml = jQuery('.countryChanger').html();
-
-    });
+	jQuery('.countryChanger .dropdown-menu a').on('click', function(){
+		var currentCountry = jQuery(this).html();
+		if (currentCountry) {
+			jQuery(this).parents('.countryChanger').find('.countryActive').html(currentCountry);
+		}
+	});
 });
